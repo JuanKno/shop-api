5ec342a5aa5d17b62cf25ea7c6f6560401cbc304
Juan Esteban Zapata <juanes0202@gmail.com>
2020-10-14 01:31:12 -0500

    Creando rutas iniciales apiResource

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,20 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
+// Buyers
+Route::apiResource('buyers', 'Buyer\BuyerController', ['only' => ['index', 'show']]);
+
+// Categories
+Route::apiResource('categories', 'Category\CategoryController');
+
+// Products
+Route::apiResource('products', 'Product\ProductController', ['only' => ['index', 'show']]);
+
+// Sellers
+Route::apiResource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
+
+// Transaction
+Route::apiResource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
+
+// Users
+Route::apiResource('users', 'User\UserController');
